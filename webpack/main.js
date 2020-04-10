a6b02272d32dc9a0a4e357b328a2c88e826ca94e
marcushoile <marcus.hoile@gmail.com>
2020-04-10 17:38:51 +1000

    wip carousel with tiny slider

--- webpack/main.js
+++ webpack/main.js
@@ -3,7 +3,15 @@
 // its own CSS file.
 import "./css/main.scss"
 
-var Flickity = require('flickity');
-var flkty = new Flickity('.carousel', {
-  contain: true
+import { tns } from "./../node_modules/tiny-slider/src/tiny-slider"
+
+tns({
+  autoplay: true,
+  autoplayButtonOutput: false,
+  container: '.carousel',
+  controls: false,
+  items: 1,
+  nav: false,
+  slideBy: 'page',
+  speed: 400
 });
